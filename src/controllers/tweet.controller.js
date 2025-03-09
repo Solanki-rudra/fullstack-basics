862fb0465d737f2bfd7b2db9d2629bcc48c99293
Rudra-solanki <157590645+Solanki-rudra@users.noreply.github.com>
2025-03-09 20:58:06 +0530

    added controller for tweet model

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,19 +6,129 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const { content } = req.body
+    const userId = req.user?.id
+
+    if (!userId) {
+        throw new ApiError(400, "Invalid user id")
+    }
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Content for tweet is required")
+    }
+
+    const tweet = await Tweet.create({ content, owner: userId })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                tweet,
+                "Tweet created successfully"
+            )
+        )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const userId = req.user?.id
+
+    if (!userId) {
+        throw new ApiError(400, "Invalid user id")
+    }
+
+    const tweets = await Tweet.find({ owner: userId })
+        .sort({ createdAt: -1 })
+
+    if (tweets.length === 0) {
+        throw new ApiError(404, "No tweets found")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                tweets,
+                "Tweets fetched successfully"
+            )
+        )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const { tweetId } = req.params
+    const userId = req.user?.id
+    const { content } = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    if (!userId) {
+        throw new ApiError(400, "Invalid user id")
+    }
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Tweet content is required")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    if (tweet.owner.toString() !== userId) {
+        throw new ApiError(400, "User not alowed to update tweet")
+    }
+
+    tweet.content = content
+    await tweet.save()
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                tweet,
+                "Tweet updated successfully"
+            )
+        )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const { tweetId } = req.params
+    const userId = req.user?.id
+
+    if (!mongoose.Types.ObjectId.isValid(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    if (!userId) {
+        throw new ApiError(400, "Invalid user id")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found while deleting")
+    }
+
+    if (tweet.owner.toString() !== userId) {
+        throw new ApiError(403, "User not allowed to delete tweet")
+    }
+
+    await tweet.deleteOne()
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Tweet deleted successfully"
+            )
+        )
 })
 
 export {
