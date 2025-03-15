bbb97b210e80effac21f6332bac65568f4936bca
Rudra-solanki <157590645+Solanki-rudra@users.noreply.github.com>
2025-03-16 01:32:40 +0530

    fix error message applying middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { ApiError } from "./utils/ApiError.js";
 
 const app = express();
 
@@ -8,6 +9,26 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
+app.use((err, req, res, next) => {
+    console.error("Error:", err);
+
+    if (err instanceof ApiError) {
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+            errors: err.errors || [],
+        });
+    }
+
+    // Handle other errors
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+        errors: [],
+    });
+});
+
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
